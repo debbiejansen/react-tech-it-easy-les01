eeed51d5ebbd38134511fed3fac19d3cf77f3f8d
debbiejansen <debbiejansen94@hotmail.com>
2025-11-16 15:01:46 +0100

    opdracht 1e gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import './App.css';
 import './helpers/totalSales.js';
 import {totalSales} from './helpers/totalSales.js';
 import {totalBought} from './helpers/totalBought.js';
+import {totalToSell} from "./helpers/totalSales.js";
 
 function App() {
-
-
     return (
         <>
             <h1>Tech it easy dashboard</h1>
@@ -20,6 +19,11 @@ function App() {
                 <article className="product">
                     <p className="product-description">Aantal verkochte producten</p>
                     <h3 className="product-total-bought">{totalBought}</h3>
+                </article>
+
+                <article className="product">
+                    <p className="product-description">Aantal verkochte producten</p>
+                    <h3 className="product-total-to-sell">{totalToSell}</h3>
                 </article>
 
             </main>
--- src/helpers/totalSales.js
+++ src/helpers/totalSales.js
@@ -8,3 +8,14 @@ function totalSalesFromInventory() {
     return totalSales;
 }
 export const totalSales = totalSalesFromInventory();
+
+
+
+function totaltoSellFromInventory() {
+    let totalToSell = 0;
+    for (let i = 0; i < inventory.length; i++) {
+        totalToSell += inventory[i].originalStock - inventory[i].sold;
+    }
+    return totalToSell;
+}
+export const totalToSell = totaltoSellFromInventory();
